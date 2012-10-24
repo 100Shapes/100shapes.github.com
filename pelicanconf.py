71839d452a1fb3fab28bf52db958429b527c2cbb
Michele Memoli <michele@onehundredshapes.com>
2012-10-24 09:44:13 +0100

    Removed language codes from urls and changed available feeds.

--- pelicanconf.py
+++ pelicanconf.py
@@ -7,20 +7,22 @@ SITEURL = ''
 
 
 # Sort this lang stuff out another time
-# ARTICLE_URL = 'en/blog/'
-# ARTICLE_SAVE_AS = 'en/blog/{date:%Y}-{date:%m}-{slug}.html'
+ARTICLE_URL = 'blog/'
+ARTICLE_SAVE_AS = 'blog/{date:%Y}-{date:%m}-{slug}.html'
 # ARTICLE_LANG_URL = '{lang}/blog/'
 # ARTICLE_LANG_SAVE_AS = '{lang}/blog/{date:%Y}-{date:%m}-{slug}.html'
-# PAGE_URL = 'en/{slug}/'
-# PAGE_SAVE_AS = 'en/{slug}/index.html'
+PAGE_URL = '{slug}/'
+PAGE_SAVE_AS = '{slug}/index.html'
 # PAGE_LANG_URL = '{lang}/{slug}/'
 # PAGE_LANG_SAVE_AS = '{lang}/{slug}/index.html'
 
+TRANSLATION_FEED = 'feeds/100shapes-%s.atom.xml'
 
-# Turn off pages we don't want
+# Turn off things we don't want
 AUTHOR_SAVE_AS = False
 CATEGORY_SAVE_AS = False
-
+CATEGORY_FEED_ATOM = False
+FEED_ATOM = False
 
 TIMEZONE = 'Europe/London'
 

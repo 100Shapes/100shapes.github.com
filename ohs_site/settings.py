12d8b5e06c7d31b287963726928a805b3ed25a0e
Michele Memoli <michele@onehundredshapes.com>
2012-12-01 18:49:29 +0000

    Renamed static files

--- ohs_site/settings.py
+++ ohs_site/settings.py
@@ -73,7 +73,7 @@ STATICBLOG_COMPILE_DIRECTORY = os.path.join(BUILD_DIR, 'blog', '')
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(SITE_ROOT, 'static')
+STATIC_ROOT = os.path.join(SITE_ROOT, '.sitestatic')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -81,7 +81,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    os.path.join(SITE_ROOT, 'globals'),
+    os.path.join(SITE_ROOT, 'static'),
 )
 
 # List of finder classes that know how to find static files in

ee861a747eae2e1c09c06008406a75e5deae52bc
Michele Memoli <michele@onehundredshapes.com>
2012-12-01 23:48:22 +0000

    Added upload to github pages for deploy

--- fabfile.py
+++ fabfile.py
@@ -4,9 +4,10 @@ from fabric.contrib import django
 django.project('ohs_site')
 from django.conf import settings
 
-import os
+import os, shutil
 
 BUILD_DIR = settings.BUILD_DIR
+BUILT_STATIC_DIR = os.path.join(BUILD_DIR, 'static')
 
 def setup():
 	local('cp ohs_site/offline/secret_settings.py ohs_site/')
@@ -32,5 +33,7 @@ def build():
 def deploy():
 	os.environ['PRODUCTION'] = '1'
 	build()
+	shutil.rmtree(BUILT_STATIC_DIR)
+	local('ghp-import %s' % BUILD_DIR)
 	del os.environ['PRODUCTION']
 	abort("Deploying isn't set up yet. Fix me.")

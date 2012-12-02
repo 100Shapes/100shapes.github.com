87c08fda5729c21ca8b31e9703c4ee34e1bec798
Michele Memoli <michele@onehundredshapes.com>
2012-12-02 00:33:09 +0000

    Typo in fabfile

--- fabfile.py
+++ fabfile.py
@@ -12,32 +12,32 @@ def setup():
 	local('cp ohs_site/offline/secret_settings.py ohs_site/')
 	local('nano ohs_site/secret_settings.py')
 
-def build_site(assets=True):
-	if not assets:
-	    local("python manage.py build")
+def build_site(include_assets=True):
+	if include_assets:
+	 	local("python manage.py build")
 	else:
-	    local("python manage.py build --skip-media --skip-static")
+		local("python manage.py build --skip-media --skip-static")
 	
-
+		
 def build_extras():
 	if not os.path.exists(BUILD_DIR):
-	    os.makedirs(BUILD_DIR)
+		os.makedirs(BUILD_DIR)
 	local('cp ohs_site/extras/* %s' % BUILD_DIR)
 
 def build_blog():
 	blog = settings.STATICBLOG_COMPILE_DIRECTORY
 	if not os.path.exists(blog):
-	    os.makedirs(blog)
+		os.makedirs(blog)
 	local("python manage.py update_blog")
 
-def build(assets):
-	build_site(assets)
+def build(include_assets=True):
+	build_site(include_assets)
 	build_blog()
 	build_extras()
 
 def deploy():
 	os.environ['PRODUCTION'] = '1'
 	build(False)
-	local('ghp-import %s' % BUILD_DIR, capture=True)
+	local('ghp-import -p %s' % BUILD_DIR, capture=True)
 	del os.environ['PRODUCTION']
 	

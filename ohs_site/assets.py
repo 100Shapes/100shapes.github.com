c991b940a23dc2a918ca571e8c7df2a2b4a9aff1
Michele Memoli <michele@onehundredshapes.com>
2012-12-02 16:11:25 +0000

    Removed modernizer from base_js bundle

--- ohs_site/assets.py
+++ ohs_site/assets.py
@@ -2,7 +2,6 @@ from django_assets import Bundle, register
 
 js_base = Bundle(
 	'js/vendor/jquery-1.8.2.min.js',
-	'js/vendor/modernizr-2.6.1.min.js',
 	output='gen/base.js')
 
 register('js_base', js_base)

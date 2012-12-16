65a30d781a19a3384fbd7f51d7a587bc6229dfb5
Michele Memoli <michele@onehundredshapes.com>
2012-12-16 22:04:39 +0000

    Fixed carousel controls

--- ohs_site/assets.py
+++ ohs_site/assets.py
@@ -7,12 +7,8 @@ js_base = Bundle(
 styles_base = Bundle(
 	'css/reset.css',
 	'css/base.css',
-	output='gen/base.%(version)s.css')
-
-styles_responsive = Bundle(
 	'css/responsive.css',
-	output='gen/responsive.%(version)s.css')
+	output='gen/base.%(version)s.css')
 
 register('js_base', js_base)
 register('css_base', styles_base)
-register('css_responsive', styles_responsive)
